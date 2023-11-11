c101ff92c5ecbd1cb02fa6f0d0e9bb7ec8d53f13
Jesus M <jesbenmel@gmail.com>
2023-11-10 20:52:59 -0600

    changes need to be made

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ function createGrid(size) {
   container.innerHTML = '';
   container.style.gridTemplateColumns = `repeat(${size}, 20px)`;
   container.style.gridTemplateRows = `repeat(${size}, 20px)`;
+  const colorPicker = document.getElementById('colorPicker');
+  const selectedColorDiv = document.getElementById('selectedColor');
+
+  let currentColor = colorPicker.value;
 
   for (let i = 0; i < size * size; i++) {
     const square = document.createElement('div');
@@ -34,8 +38,16 @@ function changeGridSize() {
   };
 };
 
+function updateSelectedColor() {
+  currentColor = colorPicker.value;
+  selectedColorDiv.style.backgroundColor = currentColor;
+}
+
 // Initial grid creation
 createGrid(16);
 
 // Assigning the click event to the button
 changeSizeBtn.addEventListener('click', changeGridSize);
+
+// Assigning the input event to the color picker
+colorPicker.addEventListener('input', updateSelectedColor);
